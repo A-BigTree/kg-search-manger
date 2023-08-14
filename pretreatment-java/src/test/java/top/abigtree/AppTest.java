f3dfc10f8bdc50e136dee3e90de8f4ca1e2e0cfb
Shuxin Wang <81904920+A-BigTree@users.noreply.github.com>
2023-08-14 23:17:33 +0800

    Update AppTest.java

--- pretreatment-java/src/test/java/top/abigtree/AppTest.java
+++ pretreatment-java/src/test/java/top/abigtree/AppTest.java
@@ -27,15 +27,6 @@ public class AppTest {
     }
 
     @Test
-    public void json2Object1() throws JsonProcessingException {
-        String json = "{\"pageid\":186,\"ns\":0,\"title\":\"Q60\",\"lastrevid\":1928151088,\"modified\":\"2023-07-03T07:45:27Z\",\"type\":\"item\",\"id\":\"Q60\",\"label\":{\"en\":{\"language\":\"en\",\"value\":\"New York City\"}},\"descriptions\":{\"en\":{\"language\":\"en\",\"value\":\"most populous city in the United States of America\"}},\"aliases\":{\"en\":[{\"language\":\"en\",\"value\":\"NYC\"},{\"language\":\"en\",\"value\":\"the five boroughs\"},{\"language\":\"en\",\"value\":\"Big Apple\"},{\"language\":\"en\",\"value\":\"City of New York\"},{\"language\":\"en\",\"value\":\"NY City\"},{\"language\":\"en\",\"value\":\"New York, New York\"},{\"language\":\"en\",\"value\":\"New York City, New York\"},{\"language\":\"en\",\"value\":\"New York, NY\"},{\"language\":\"en\",\"value\":\"New York City, NY\"},{\"language\":\"en\",\"value\":\"Caput Mundi\"},{\"language\":\"en\",\"value\":\"The City So Nice They Named It Twice\"},{\"language\":\"en\",\"value\":\"Capital of the World\"},{\"language\":\"en\",\"value\":\"New York\"},{\"language\":\"en\",\"value\":\"New York City (NYC)\"},{\"language\":\"en\",\"value\":\"New York (city)\"},{\"language\":\"en\",\"value\":\"city of New York\"},{\"language\":\"en\",\"value\":\"Center of the Universe\"}]}}";
-
-        WikiDataModel wikiDataModel = mapper.readValue(json, WikiDataModel.class);
-        log.info(wikiDataModel.toString());
-
-    }
-
-    @Test
     public void json2Object2() throws IOException {
         URL url = this.getClass().getClassLoader().getResource("test.json");
         WikiDataModel wikiDataModel = mapper.readValue(url, WikiDataModel.class);
