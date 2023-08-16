123f1e57a7a535659181e5cfb80d082e1989848a
Shuxin-Wang <81904920+A-BigTree@users.noreply.github.com>
2023-08-16 15:14:24 +0800

    Update ValueTypeEnum.java

--- pretreatment-java/src/main/java/top/abigtree/wiki/enums/ValueTypeEnum.java
+++ pretreatment-java/src/main/java/top/abigtree/wiki/enums/ValueTypeEnum.java
@@ -2,37 +2,41 @@ package top.abigtree.wiki.enums;
 
 import java.util.HashMap;
 
+import lombok.Getter;
+
 /**
  * @author Shuxin-Wang <wangshuxin662@gmail.com>
  * Created on 2023/7/6
  */
+@Getter
 public enum ValueTypeEnum {
-    UN_KNOW("un-know"),
-    STRING("string"),
-    WIKI_BASE_ENTITY_ID("wikibase-entityid"),
-    GLOBE_COORDINATE("globecoordinate"),
-    MONOLINGUAL_TEXT("monolingualtext"),
-    QUANTITY("quantity"),
-    TIME("time")
+    UN_KNOW(0, "un-know"),
+    STRING(1, "string"),
+    WIKI_BASE_ENTITY_ID(2, "wikibase-entityid"),
+    GLOBE_COORDINATE(3, "globecoordinate"),
+    MONOLINGUAL_TEXT(4, "monolingualtext"),
+    QUANTITY(5, "quantity"),
+    TIME(6, "time")
     ;
 
     final static HashMap<String, ValueTypeEnum> mapper = new HashMap<>();
+    final static HashMap<Integer, ValueTypeEnum> idMapper = new HashMap<>();
 
     static {
         for(ValueTypeEnum valueType:ValueTypeEnum.values()){
             mapper.put(valueType.getType(), valueType);
+            idMapper.put(valueType.getTypeId(), valueType);
         }
     }
 
 
+    final Integer typeId;
+
     final String type;
 
-    ValueTypeEnum(String type){
+    ValueTypeEnum(Integer id, String type){
+        this.typeId = id;
         this.type = type;
-    }
-
-    public String getType(){
-        return type;
     }
 
     public static ValueTypeEnum getValueType(String type){
